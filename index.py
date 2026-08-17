abba388dd315f37728ddb8330c80faa964d90076
VINH <vinhchann1106@gmail.com>
2026-08-17 18:31:45 +0700

    fix: change user-agent

--- index.py
+++ index.py
@@ -115,7 +115,7 @@ class ACBClient:
         self.http.mount("https://", HTTPAdapter(max_retries=retry))
         self.http.headers.update({
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-                          "(KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36 Edg/142.0.0.0",
+                          "(KHTML, like Gecko) Chrome/151.0.0.0 Safari/537.36",
             "Accept-Language": "en-GB,en;q=0.9,en-US;q=0.8,vi;q=0.7",
         })
 
